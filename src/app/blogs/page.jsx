3659a5e0c30197245c5059bcedb33b2422cafb6e
shahinexy <shahinexy@gmail.com>
2025-01-22 19:16:00 +0600

    Ts Type Guard Blog added

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import react from "../../../public/images/1_JdkEvoNMusSGEuuqUZ0OLg.jpg";
 import { MdArrowOutward } from "react-icons/md";
+import typeScript from "../../../public/images/ts-type-guard.png";
 
 const BlogPage = () => {
   return (
@@ -14,7 +15,7 @@ const BlogPage = () => {
         <span className="text-secondary font-normal">&#47;&#47; </span> Blogs
         <span className="text-secondary font-normal"> &#47;&#47; </span>
       </h1>
-      <div className="grid md:grid-cols-3 grid-cols-1 text-center mt-12">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-5 text-center mt-12">
         <div class="relative h-[350px] group duration-500 cursor-pointer group overflow-hidden text-gray-50 hover:duration-700 ">
           <div>
             <Image
@@ -33,6 +34,33 @@ const BlogPage = () => {
             <span class="flex justify-center mt-3">
               <Link
                 href={`https://www.linkedin.com/pulse/what-react-why-should-we-use-front-end-shahin-alam-t57ue/?trackingId=6tvFdsQxQ1a%2BCMtrhFRc%2Fw%3D%3D`}
+                target="_blank"
+              >
+                <button className="flex gap-2 items-center text-xl text-primary hover:bg-primary hover:text-white duration-500 border border-primary px-7 py-1">
+                  View <MdArrowOutward />{" "}
+                </button>
+              </Link>
+            </span>
+          </div>
+        </div>
+        <div class="relative h-[350px] group duration-500 cursor-pointer group overflow-hidden text-gray-50 hover:duration-700 ">
+          <div>
+            <Image
+              className="h-72"
+              src={typeScript}
+              alt="Logo"
+              width={800}
+              height={600}
+            />
+          </div>
+
+          <div class="absolute bg-gray-50 -bottom-16 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
+            <span class="text-primary font-bold text-xl">
+              The Importance of Type Guards in TypeScript: Types & Use Cases
+            </span>
+            <span class="flex justify-center mt-3">
+              <Link
+                href={`https://www.linkedin.com/pulse/importance-type-guards-typescript-types-use-cases-shahin-alam-cwgye/?trackingId=U3wgWOjqlegdIOq2sap%2Bjw%3D%3D`}
                 target="_blank"
               >
                 <button className="flex gap-2 items-center text-xl text-primary hover:bg-primary hover:text-white duration-500 border border-primary px-7 py-1">
